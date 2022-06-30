8694889e490eb6e1c5a624456b04aabacc94942e
Nick <mr.upgrade3@protonmail.com>
2022-06-30 23:41:18 +0300

    Make HW #2 - Делаем таски

---
+++ HM #2 - Делаем таски/Task #2/script/script.js
@@ -0,0 +1,2 @@
+const earthPlanet = "Earth"; // Name for variable which contain name of our planet
+let currentUser; // Name for variable which contain name of current user
---
+++ HM #2 - Делаем таски/Task #3/script/script.js
@@ -0,0 +1,10 @@
+const strTen = "10" + 10;
+console.log(typeof strTen);
+
+/*
+   We are take one type (string) and plus type (number).
+   In this case number 10 will change type for string, so we
+   get type string of final value and this work not like usual
+   math operation, so finally constant strTen equal "1010".
+
+ */
