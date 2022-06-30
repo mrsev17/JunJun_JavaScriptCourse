e6e23b1e5881e5a2b039f644ea76dce1f0a87287
Nick <mr.upgrade3@protonmail.com>
2022-06-30 23:46:14 +0300

    Update HW #2. Add readme file with description of tasks.

--- HM #2 - Делаем таски/Task #3/script/script.js
+++ HM #2 - Делаем таски/Task #3/script/script.js
@@ -6,5 +6,4 @@ console.log(typeof strTen);
    In this case number 10 will change type for string, so we
    get type string of final value and this work not like usual
    math operation, so finally constant strTen equal "1010".
-
- */
+*/
