13ee6f661eb5bc879224ae1a73f25b30051b77b2
Nick <mr.upgrade3@protonmail.com>
2022-07-05 00:47:44 +0300

    Make HW #5 - Objects

---
+++ HW #5 - Обьекты/Task #2/script/script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const user = {
+    name: 'John'
+};
+
+// это будет работать?
+
+user.name = 'Pete';
+
+// According to task #1 from homework #5
+// we can see example how its work, so yes it work
---
+++ HW #5 - Обьекты/Task #3/script/script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130
+};
+
+const sum = salaries.John + salaries.Ann + salaries.Pete;
+console.log("Salary of all workers equal " + sum + "$");
