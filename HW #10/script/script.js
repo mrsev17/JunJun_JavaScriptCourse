674845cb719890b5f657806efe9308e6dea16ccf
Nick <mr.upgrade3@protonmail.com>
2022-07-12 21:59:08 +0300

    Udd original script to HW #10, cause in readme showing bad

--- HW #10/script/script.js
+++ HW #10/script/script.js
@@ -1,5 +1,23 @@
 "use strict";
 
+/* Original script */
+
+/*
+let numOrStr = prompt('input number or string');
+console.log(numOrStr)
+
+if(numOrStr === null) {
+    console.log('вы отменили')
+} else if( numOrStr.trim() === '' ) {
+    console.log('Empty String');
+} else if( isNaN( +numOrStr ) ) {
+    console.log(' number is Ba_NaN')
+} else {
+    console.log('OK!')
+}
+*/
+
+
 const numOrStr = prompt("input number or string");
 
 switch (true) {
