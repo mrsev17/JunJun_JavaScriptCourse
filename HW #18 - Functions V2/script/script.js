d90183b1330cedc7b8de179a5c566d7a68884a3d
Nick <mr.upgrade3@protonmail.com>
2022-07-22 00:07:58 +0300

    Update HW #18 (Solution from lesson)

--- HW #18 - Functions V2/script/script.js
+++ HW #18 - Functions V2/script/script.js
@@ -1,66 +1,20 @@
 "use strict";
-/*
-    Вам нужно написать реализацию функции padString, которая принимает 4 аргумента:
 
-    1. строку
+const padString = (initialString, estimatedLength, symbol, right = true) => {
 
-    2. число, которое является длинной строки, которую мы хотим получить в результате выполнения функции
+    if(typeof initialString !== "string") throw new Error("InitialString is not in type String");
+    if(typeof estimatedLength !== "number") throw new Error("InitialString is not in type Number");
 
-    3. символ (строка длинной 1 символ) — которым дополнится строка, если это будет необходимо
+    if(initialString.length === estimatedLength) return initialString;
+    if(initialString.length > estimatedLength) return initialString.substring(0,estimatedLength);
 
-    4. параметр булеан (true или false), который определяет, добавлять символы слева или справа (по умолчанию справа)
+    if(typeof symbol !== "string") throw new Error("symbol is not in type String");
+    if(symbol.length > 1) throw new Error("symbol length is too long");
 
-    Обязательно при написании функции необходимо проверить аргументы, которые мы передали,
-    и если каких-то аргументов нет, то вернуть из функции строку с ошибкой (return ‘some error’).
-    Сообщение с ошибкой должно быть разное в зависимости от того условия, по которому не прошла проверка.
+    if(typeof right !== "boolean") throw new Error("Side length is too Boolean");
 
-    Результат вызова функции нужно вывести в консоль — после завершения функции.
+    const symbols = symbol.repeat(estimatedLength - initialString.length);
 
-    Например:
+    return right ? initialString + symbols : symbols + initialString;
 
-    Вызов padString(‘hello’, 8, ‘*’) вернет строку hello***
-
-    А вызов padString(‘hello’, 6, ‘*’, false) вернет строку *hello
-
-    Вызов padString(‘hello’, 2) вернет ‘he’ — если число меньше, чем
-    размер введенной строки, нужно строку обрезать при помощи метода subst
-*/
-
-const padString = (str,leng,symb = "",side = false) => {
-
-    if (typeof str !== "string") {
-        return "Error: First data must be a string";
-    } else if (typeof leng !== "number") {
-        return "Error: Second data must be a number";
-    }
-
-    let switchSide;
-    if (side === true) {
-        switchSide = 1;
-    } else if (side === false) {
-        switchSide = 0;
-    } else if (side === "undefined") {
-        switchSide = 0;
-    } else if (side === String(side)) {
-        return "Error: Fourth data must be true or false"
-    }
-
-    const mainTarget = () => {
-        if ((leng >= str.length) && (switchSide === 0) && (symb.length === 1)) {
-            let plsStr;
-            let symbRep = symb.repeat(leng);
-            plsStr = str + symbRep;
-            return plsStr;
-        } else if (leng < str.length) {
-            return str.substr(str[length],leng);
-        } else if ((leng >= str.length) && (switchSide === 1) && (symb.length === 1)) {
-            let leftStr;
-            let symbRepeat = symb.repeat(leng);
-            leftStr = symbRepeat + str;
-            return leftStr;
-        }
-    };
-
-    return mainTarget();
-
-};
+}
