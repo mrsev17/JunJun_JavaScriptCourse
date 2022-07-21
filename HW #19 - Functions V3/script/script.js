da2293c45689093c0e30b50decd4b39b604e1812
Nick <mr.upgrade3@protonmail.com>
2022-07-21 20:36:56 +0300

    Update HW #18 (Solution from lesson)

--- HW #19 - Functions V3/script/script.js
+++ HW #19 - Functions V3/script/script.js
@@ -1,52 +1,25 @@
 "use strict";
-/*
-    Дан массив с числами const arr = [1, 2, 3, -1, -2, -3];
 
-    Необходимо написать функцию которая вернет новый массив
-    содержащий только положиельные числа. Для этого:
+const arr = [1,2,3, -1, -2, -3];
 
-    — Создаем функцию с произвольным именем
+const getPositiveNumbers = initialArray => {
 
-    — Функция принимает массив в качестве параметра
+    const defaultResult = [];
 
-    — В теле функции создаем пустой массив (пример: const exampleArr = [])
+    if( !Array.isArray(initialArray) || !initialArray.length ) return defaultResult;
 
-    — Если массив переданный в параметр функции пустой, то возвращаем сообщение об этом
+    const positiveNumbersArray = [];
 
-    — Перебираем циклом массив, который передан в качестве параметра (использовать цикл for)
+    for(let i = 0; i < initialArray.length; i +=1) {
+        if(typeof initialArray[i] !== "number") continue;
+        if(isNaN(initialArray[i])) continue;
 
-    — В теле цикла проверям является ли текущий элемент положительным числом или отрицательным (if)
-
-    — Если число положительное, то добавлем его в ранее созданный массив с помощь функции push (пример exampleArr.push(currentPositiveNumber))
-
-    — После выполнения цикла проверям массив, который наполняли только положиельными значениями
-
-    — Если он не пустой, то возвращаем этот массив
-
-    — Если пустой, возвращаем null
-*/
-
-const positiveValue = (arrNumbers) => {
-
-    const emptyArr = [];
-
-    if (arrNumbers === undefined) {
-        return alert("Function cannot be empty.");
-    } else if (arrNumbers.length === 0) {
-        return alert("Array is empty.");
-    } else if (!(Array.isArray(arrNumbers))) {
-        return alert(arrNumbers + " is not array.");
-    }
-
-    for (let i = 0; i < arrNumbers.length; i++) {
-        if (arrNumbers[i] > 0) {
-            emptyArr.push(arrNumbers[i]);
+        if( initialArray[i] > 0) {
+            positiveNumbersArray.push(initialArray[i])
         }
     }
-
-    if (emptyArr.length === 0) {
-        return alert("Result is " + null);
-    }
-    return alert(emptyArr);
-
+    return positiveNumbersArray.length > 0 ? positiveNumbersArray : null;
 };
+
+const positiveNumbersArray = getPositiveNumbers(arr);
+console.log(positiveNumbersArray);
