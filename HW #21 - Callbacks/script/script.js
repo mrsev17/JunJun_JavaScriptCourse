5620648421da3ee2e1b4568d32f1e3bccc29495b
Nick <mr.upgrade3@protonmail.com>
2022-07-30 02:04:55 +0300

    Make HW #21

---
+++ HW #21 - Callbacks/script/script.js
@@ -0,0 +1,30 @@
+const exponentiation = (first,second) => {
+    const resultExponentiation = Math.pow(first,second);
+    return alert("Result of exponentiation " + first + " and " + second + " is a " + resultExponentiation);
+}
+
+const multiplay = (first,second) => {
+    const resultMultiplay = first * second;
+    return alert("Result of multiplay " + first + " and " + second + " is a " + resultMultiplay);
+}
+
+const division = (first,second) => {
+    const resultDivision = first / second;
+    return alert("Result of division " + first + " and " + second + " is a " + resultDivision);
+}
+
+const modulo = (first,second) => {
+    const resultModulo = first % second;
+    return alert("Result of modulo " + first + " and " + second + " is a " + resultModulo);
+}
+
+const mainFunction = (callBack) => {
+    const first = +prompt("Enter first number:");
+    const second = +prompt("Enter second number:");
+    callBack(first,second);
+}
+
+mainFunction(exponentiation);
+mainFunction(multiplay);
+mainFunction(division);
+mainFunction(modulo);
