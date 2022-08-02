eaf44b59b3cb2fb754557d7ae1c72ddbf6f38e4f
Nick <mr.upgrade3@protonmail.com>
2022-08-03 02:25:34 +0300

    Complete task 2 from HW #25

--- HW #25 - Array Methods V3/script/script.js
+++ HW #25 - Array Methods V3/script/script.js
@@ -17,3 +17,27 @@ const copyIndexOf = (arrayWork,searchElement,fromIndex = 0) => {
 }
 
 
+
+const copyLastIndexOf = (string,value,fromIndex) => {
+
+    let from = string.length;
+    let userFromIndex = undefined;
+    let index = -1;
+
+    if (fromIndex <= 0) return -1;
+    if (fromIndex < string.length) userFromIndex = fromIndex;
+
+    for (let i = 0; i < userFromIndex; i += 1) {
+        if (string[i] === value) {
+            return i;
+        }
+    }
+
+    for (let i = 0; i < string.length; i += 1) {
+        if (string[i] === value) {
+            index = i;
+        }
+    }
+    return index;
+}
+
